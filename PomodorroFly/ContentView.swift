40a76ad3905c34f00188b3ddb41b6da0c7943e94
Andrei Musca <andrei.m150@yahoo.com>
2025-11-05 18:00:20 +0200

    Initial Commit

---
+++ PomodorroFly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PomodorroFly
+//
+//  Created by Andrei Musca on 05.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PomodorroFly/PomodorroFlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodorroFlyApp.swift
+//  PomodorroFly
+//
+//  Created by Andrei Musca on 05.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct PomodorroFlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
